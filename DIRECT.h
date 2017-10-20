177a4d458e73ecdf0546a0c0e0fcc89096a1152f
Anatol1987 <anatolij.trush@yandex.ru>
2017-10-20 16:57:31 +0300

    from work

--- DIRECT.h
+++ DIRECT.h
@@ -12,31 +12,37 @@ void gyro_stab(uint16_t(&power)[4]) {
 
 }
 void axis_x_l() {
-	for (uint8_t i = 0; i < 4; i++)
-	{
-	}
+	//for (uint8_t i = 0; i < 4; i++)
+	//{
+	//}
 }
 void axis_y_l() {
-	for (uint8_t i = 0; i < 4; i++)
+	//for (uint8_t i = 0; i < 4; i++)
+	//{
+	//}
+}
+void axis_x_r(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {//CHANGE_power[0]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	if (data_upr[2] >= 127)
 	{
+		power[0] = standart[0] + map(data_upr[1], 127, 255, 0, 1400);
+		power[0] = standart[0] + map(data_upr[1], 127, 255, 0, 1400);
+	}
+	else if (data_upr[2] < 127)
+	{
+		power[0] = standart[0] + map(data_upr[1], 127, 0, 0, 1400);
+		power[0] = standart[0] + map(data_upr[1], 127, 0, 0, 1400);
 	}
 }
-void axis_x_r(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
-	for (uint8_t i = 0; i < 4; i++)
+void axis_y_r(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {//CHANGE_power[0]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	if (data_upr[3] >= 127)
 	{
-		if (data_upr[2] >= 127)
-			power[i] = standart[i] + map(data_upr[1], 127, 255, 0, 1400);
-		else if (data_upr[2] < 127)
-			power[i] = standart[i] - map(data_upr[1], 127, 255, 0, 1400);
+		power[0] = standart[0] + map(data_upr[1], 127, 255, 0, 1400);
+		power[0] = standart[0] + map(data_upr[1], 127, 255, 0, 1400);
 	}
-}
-void axis_y_r(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
-	for (uint8_t i = 0; i < 4; i++)
+	else if (data_upr[3] < 127)
 	{
-		if (data_upr[3] >= 127)
-			power[i] = standart[i] + map(data_upr[1], 127, 255, 0, 1400);
-		else if (data_upr[3] < 127)
-			power[i] = standart[i] - map(data_upr[1], 127, 255, 0, 1400);
+		power[0] = standart[0] + map(data_upr[1], 127, 0, 0, 1400);
+		power[0] = standart[0] + map(data_upr[1], 127, 0, 0, 1400);
 	}
 }
 void left_joystick(uint16_t(&power)[4]) {
