654adc960b8e59676595e69071a389154a60fd99
Anatol1987 <anatolij.trush@yandex.ru>
2017-10-27 13:28:04 +0300

    KREN+TANG

--- DIRECT.h
+++ DIRECT.h
@@ -8,19 +8,86 @@
 #define MIN_POWER 800
 #define MAX_POWER 2300
 #define POWER_TO_START 880
-#define POWER_TO_UP 1200
-#define POWER_TANG_KREN 500
-#define POWER_YAW 500
+#define POWER_TO_UP 1400
+#define POWER_TANG_KREN 250
+#define POWER_YAW 250
 #define POWER_FOR_PERIOD 5
 #define PERIOD 20
+#define YAW_PERC 0.85
 
 uint16_t standart[4] = { MIN_POWER, MIN_POWER, MIN_POWER, MIN_POWER };
 unsigned long currr = 0;
 unsigned long prevvv = 0;
 unsigned long _currr = 0;
 unsigned long _prevvv = 0;
+float gyro_coeff_X = 1;
+float gyro_coeff_Y = 1;
 uint8_t add = 0;
 
+void yaw(uint16_t(&power)[4]) {
+	power[0] *= 1 + (1 - YAW_PERC);
+	power[3] *= 1 + (1 - YAW_PERC);
+	power[1] *= YAW_PERC;
+	power[2] *= YAW_PERC;
+}
+void correct(uint16_t(&power)[4]) {
+	//180
+	if (Gyro::diffX>180 && Gyro::diffY == 180)
+	{
+		power[0] = standart[0] * (gyro_coeff_X*gyro_coeff_X);
+		power[1] = standart[1] * (gyro_coeff_X*gyro_coeff_X);
+		power[2] = standart[2];
+		power[3] = standart[3];
+	}
+	else if (Gyro::diffX<180 && Gyro::diffY == 180) {
+		power[2] = standart[2] * (gyro_coeff_X*gyro_coeff_X);
+		power[3] = standart[3] * (gyro_coeff_X*gyro_coeff_X);
+		power[0] = standart[0];
+		power[1] = standart[1];
+	}
+	else if (Gyro::diffX == 180 && Gyro::diffY<180) {
+		power[0] = standart[0];
+		power[1] = standart[1] * (gyro_coeff_Y*gyro_coeff_Y);
+		power[2] = standart[2] * (gyro_coeff_Y*gyro_coeff_Y);
+		power[3] = standart[3];
+	}
+	else if (Gyro::diffX == 180 && Gyro::diffY>180) {
+		power[0] = standart[0] * (gyro_coeff_Y*gyro_coeff_Y);
+		power[1] = standart[1];
+		power[2] = standart[2];
+		power[3] = standart[3] * (gyro_coeff_Y*gyro_coeff_Y);
+	}
+	//not 180
+	else if (Gyro::diffX>180 && Gyro::diffY>180)
+	{
+		power[0] = standart[0] * (gyro_coeff_X*gyro_coeff_X);
+		power[1] = standart[1] * (gyro_coeff_X*gyro_coeff_X);
+		power[3] = standart[3] * (gyro_coeff_Y*gyro_coeff_Y);
+		power[0] *= (gyro_coeff_Y*gyro_coeff_Y);
+		power[2] = standart[2];
+	}
+	else if (Gyro::diffX<180 && Gyro::diffY<180) {
+		power[2] = standart[2] * (gyro_coeff_X*gyro_coeff_X);
+		power[3] = standart[3] * (gyro_coeff_X*gyro_coeff_X);
+		power[1] = standart[1] * (gyro_coeff_Y*gyro_coeff_Y);
+		power[2] *= (gyro_coeff_Y*gyro_coeff_Y);
+		power[0] = standart[0];
+	}
+	else if (Gyro::diffX>180 && Gyro::diffY<180) {
+		power[0] = standart[0] * (gyro_coeff_X*gyro_coeff_X);
+		power[1] = standart[1] * (gyro_coeff_X*gyro_coeff_X);
+		power[2] = standart[2] * (gyro_coeff_Y*gyro_coeff_Y);
+		power[1] *= (gyro_coeff_Y*gyro_coeff_Y);
+		power[3] = standart[3];
+	}
+	else if (Gyro::diffX<180 && Gyro::diffY>180) {
+		power[2] = standart[2] * (gyro_coeff_X*gyro_coeff_X);
+		power[3] = standart[3] * (gyro_coeff_X*gyro_coeff_X);
+		power[0] = standart[0] * (gyro_coeff_Y*gyro_coeff_Y);
+		power[3] *= (gyro_coeff_Y*gyro_coeff_Y);
+		power[1] = standart[1];
+	}
+}
 void height_up(uint8_t value) {
 	currr = millis();
 	if (currr - prevvv >= PERIOD)
@@ -39,28 +106,39 @@ void height_down(uint8_t value) {
 		_prevvv = _currr; //remember time
 	}
 }
-void gyro_stab(uint16_t(&power)[4]) {
-
+void gyro_stab(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
+	if (standart[0] >= POWER_TO_UP && standart[1] >= POWER_TO_UP && standart[2] >= POWER_TO_UP && standart[3] >= POWER_TO_UP)
+	{
+		gyro_coeff_X = 1 + (abs(Gyro::diffX - 180) / 100);
+		gyro_coeff_Y = 1 + (abs(Gyro::diffY - 180) / 100);
+		correct(power);
+	}
+#ifdef DEBUG
+	//Serial.print("gyro_coeff_X: ");
+	//Serial.println(gyro_coeff_X);
+#endif // DEBUG
 }
 //LEFT_X
 void axis_x_l(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
-	if (standart[0] > POWER_TO_UP && standart[1] > POWER_TO_UP && standart[2] > POWER_TO_UP && standart[3] > POWER_TO_UP)
-	{
+	//if (standart[0] > POWER_TO_UP && standart[1] > POWER_TO_UP && standart[2] > POWER_TO_UP && standart[3] > POWER_TO_UP)
+	//{
+	//yaw(power);
 		if (data_upr[0] >= 127)
 		{
-			power[0] += map(data_upr[0], 127, 255, 0, POWER_YAW);
-			power[2] += map(data_upr[0], 127, 255, 0, POWER_YAW);
-			power[1] -= map(data_upr[0], 127, 255, 0, POWER_YAW);
-			power[3] -= map(data_upr[0], 127, 255, 0, POWER_YAW);
+			power[0] += map(data_upr[0], 127, 0, 0, POWER_YAW);
+			power[3] += map(data_upr[0], 127, 0, 0, POWER_YAW);
+			power[1] -= map(data_upr[0], 127, 0, 0, POWER_YAW);
+			power[2] -= map(data_upr[0], 127, 0, 0, POWER_YAW);
+
 		}
 		else if (data_upr[0] < 127)
 		{
-			power[1] += map(data_upr[0], 127, 0, 0, POWER_YAW);
-			power[3] += map(data_upr[0], 127, 0, 0, POWER_YAW);
-			power[0] -= map(data_upr[0], 127, 0, 0, POWER_YAW);
-			power[2] -= map(data_upr[0], 127, 0, 0, POWER_YAW);
+			power[1] += map(data_upr[0], 127, 255, 0, POWER_YAW);
+			power[2] += map(data_upr[0], 127, 255, 0, POWER_YAW);
+			power[0] -= map(data_upr[0], 127, 255, 0, POWER_YAW);
+			power[3] -= map(data_upr[0], 127, 255, 0, POWER_YAW);
 		}
-	}
+	//}
 }
 //LEFT_Y
 void axis_y_l(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
@@ -88,24 +166,24 @@ void axis_y_l(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
 }
 //RIGHT_X
 void axis_x_r(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
-	if (standart[0]>POWER_TO_UP && standart[1]>POWER_TO_UP && standart[2]>POWER_TO_UP && standart[3]>POWER_TO_UP)
-	{
+	//if (standart[0]>POWER_TO_UP && standart[1]>POWER_TO_UP && standart[2]>POWER_TO_UP && standart[3]>POWER_TO_UP)
+	//{
 		if (data_upr[2] >= 127)
 		{
 			power[1] += map(data_upr[2], 127, 255, 0, POWER_TANG_KREN);
-			power[2] += map(data_upr[2], 127, 255, 0, POWER_TANG_KREN);
+			power[3] += map(data_upr[2], 127, 255, 0, POWER_TANG_KREN);
 		}
 		else if (data_upr[2] < 127)
 		{
 			power[0] += map(data_upr[2], 127, 0, 0, POWER_TANG_KREN);
-			power[3] += map(data_upr[2], 127, 0, 0, POWER_TANG_KREN);
+			power[2] += map(data_upr[2], 127, 0, 0, POWER_TANG_KREN);
 		}
-	}
+	//}
 }
 //RIGHT_Y
 void axis_y_r(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
-	if (standart[0]>POWER_TO_UP && standart[1]>POWER_TO_UP && standart[2]>POWER_TO_UP && standart[3]>POWER_TO_UP)
-	{
+	//if (standart[0]>POWER_TO_UP && standart[1]>POWER_TO_UP && standart[2]>POWER_TO_UP && standart[3]>POWER_TO_UP)
+	//{
 		if (data_upr[3] >= 127)
 		{
 			power[2] += map(data_upr[3], 127, 255, 0, POWER_TANG_KREN);
@@ -116,11 +194,11 @@ void axis_y_r(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
 			power[0] += map(data_upr[3], 127, 0, 0, POWER_TANG_KREN);
 			power[1] += map(data_upr[3], 127, 0, 0, POWER_TANG_KREN);
 		}
-	}
+	//}
 }
 //
 void left_joystick(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
-	//axis_x_l(power, data_upr);
+	axis_x_l(power, data_upr);
 	axis_y_l(power, data_upr);
 }
 void right_joystick(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
@@ -144,10 +222,11 @@ void calculate(uint16_t(&power)[4], uint8_t(&data_upr)[6]) {
 	//ASSIGNMENT
 	for (uint8_t i = 0; i < 4; i++)
 		power[i] = standart[i];
-	//CALCULATE
-	gyro_stab(power);
+	//CALCULATE	
 	left_joystick(power, data_upr);
-	//right_joystick(power, data_upr);
+	right_joystick(power, data_upr);
+
+	//gyro_stab(power, data_upr);
 	check(power);
 }
 #endif // DIRECT_H
--- KV_NEW.ino
+++ KV_NEW.ino
@@ -11,7 +11,7 @@
 #include "DIRECT.h"
 
 #define SIZE_OF_DATA 6
-#define GYRO_PERIOD 20
+#define GYRO_PERIOD 10
 //Read voltage
 #define VOLTAGE_PIN A2
 #define MIN_VOLTAGE 10//min voltage
@@ -30,7 +30,7 @@ Servo motorRL;
 uint8_t data_upr[SIZE_OF_DATA] = { 0, 0, 0, 0 , 0 , 0 }; // for test
 uint16_t power[4] = {MIN_POWER, MIN_POWER, MIN_POWER, MIN_POWER };
 
-RF24 radio(9, 10); // "создать" модуль на пинах 9 и 10
+RF24 radio(9, 10);				// "создать" модуль на пинах 9 и 10
 byte address[][6] = { "1Node", "2Node", "3Node", "4Node", "5Node", "6Node" }; //возможные номера труб
 byte pipeNo, gotByte;
 
@@ -39,46 +39,46 @@ void setup() {
 
   moto_init(motorFR, motorFL, motorRR, motorRL);
 
-  radio.begin(); //активировать модуль
-  radio.setAutoAck(1);         //режим подтверждени€ приЄма, 1 вкл 0 выкл
-  radio.setRetries(0, 5);     //(врем€ между попыткой достучатьс€, число попыток)
-  radio.enableAckPayload();    //разрешить отсылку данных в ответ на вход€щий сигнал
-  radio.setPayloadSize(32);     //размер пакета, в байтах
+  radio.begin();				//активировать модуль
+  radio.setAutoAck(1);			//режим подтверждени€ приЄма, 1 вкл 0 выкл
+  radio.setRetries(0, 5);		//(врем€ между попыткой достучатьс€, число попыток)
+  radio.enableAckPayload();		//разрешить отсылку данных в ответ на вход€щий сигнал
+  radio.setPayloadSize(32);		//размер пакета, в байтах
 
   radio.openReadingPipe(1, address[0]);      //хотим слушать трубу 0
-  radio.setChannel(0x60);  //выбираем канал (в котором нет шумов!)
+  radio.setChannel(0x60);		//выбираем канал (в котором нет шумов!)
 
-  radio.setPALevel(RF24_PA_MAX); //уровень мощности передатчика. Ќа выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
-  radio.setDataRate(RF24_1MBPS); //скорость обмена. Ќа выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
-								   //должна быть одинакова на приЄмнике и передатчике!
-								   //при самой низкой скорости имеем самую высокую чувствительность и дальность!!
-  radio.powerUp(); //начать работу
-  radio.startListening();  //начинаем слушать эфир, мы приЄмный модуль
+  radio.setPALevel(RF24_PA_MAX);	//уровень мощности передатчика. Ќа выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setDataRate(RF24_1MBPS);	//скорость обмена. Ќа выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
+									//должна быть одинакова на приЄмнике и передатчике!
+									//при самой низкой скорости имеем самую высокую чувствительность и дальность!!
+  radio.powerUp();					//начать работу
+  radio.startListening();			//начинаем слушать эфир, мы приЄмный модуль
 }
 void loop() {
-	gyro.update();
+	while (true)
+	{
+		gyro.update();
 
-	motorFR.writeMicroseconds(power[0]);
-	motorFL.writeMicroseconds(power[1]);
-	motorRR.writeMicroseconds(power[2]);
-	motorRL.writeMicroseconds(power[3]);
-	
-	if (radio.available(&pipeNo)) {
-		radio.read(&data_upr, sizeof(data_upr));
-		gotByte = volt.update() * 10;
-		radio.writeAckPayload(pipeNo, &gotByte, 1);  // отправл€ем обратно то что прин€ли
+		motorFR.writeMicroseconds(power[0]);
+		motorFL.writeMicroseconds(power[1]);
+		motorRR.writeMicroseconds(power[2]);
+		motorRL.writeMicroseconds(power[3]);
+
+		if (radio.available(&pipeNo)) {
+			radio.read(&data_upr, sizeof(data_upr));
+			gotByte = volt.update() * 10;
+			radio.writeAckPayload(pipeNo, &gotByte, 1);	// отправл€ем обратно то что прин€ли
 #ifdef DEBUG
-		delay(100);
-		Serial.print("DATA: ");
-		Serial.println(data_upr[1]);
-		Serial.println("power[1]: ");		
-		Serial.println(power[1]);
+			delay(100);
+			Serial.print("DATA: ");
+			Serial.println(data_upr[1]);
+			Serial.println("power[1]: ");
+			Serial.println(power[1]);
 #endif // DEBUG
-		//delay(10);
+			calculate(power, data_upr);
+		}
 
-		calculate(power, data_upr);
+		led.update();
 	}
-
-  led.update();
 }
-// TODO: while (true) instead of loop
